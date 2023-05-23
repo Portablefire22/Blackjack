b6971e5f4609d2fb2cb600db215fe6004a545934
Portablefire22 <barnowlsarecool@gmail.com>
2023-05-23 05:28:46 +0100

    Added comments and cleaned up the shuffling code a little bit

--- src/cards.rs
+++ src/cards.rs
@@ -1,9 +1,13 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone)] // Required to allow for the values to be cloned, and then displayed to
+                        // the screen with debug tools.
+
+// Create the frame that the cards will be placed into.
 pub struct Card {
     suit: String,
     value: String,
 }
 
+// Places the values into the card struct.
 fn build_card(cardSuit: String, cardValue: String) -> Card {
     let mut card1 = Card {
         suit: cardSuit,
@@ -14,20 +18,21 @@ fn build_card(cardSuit: String, cardValue: String) -> Card {
 
 pub fn build_deck() -> Vec<Card>{
     let mut deck = Vec::new();
-    let suites: [&str; 4] = ["Hearts","Diamonds","Spades","Clubs"];
-    let specials: [&str; 3] = ["King","Queen","Jack"];
+    let suites: [&str; 4] = ["Hearts","Diamonds","Spades","Clubs"]; // Allocates the suites
+    let specials: [&str; 3] = ["King","Queen","Jack"]; // Allocates the specials for the suites
     let mut card: Card;
-    for suite in suites {
-        for i in 1..11 {
-            deck.push(build_card(suite.to_string(), i.to_string()));
-            if (i == 10) {
-                for special in specials {
-                    deck.push(build_card(suite.to_string(), special.to_string()));
+    for suit in suites { // Iterate through every suit
+        for i in 1..11 { // Iterate through every card value from 1 to 10
+            deck.push(build_card(suit.to_string(), i.to_string())); // Build a card and add it to
+                                                                    // the deck 
+            if (i == 10) { // Building the specials
+                for special in specials { // Iterate through all specials 
+                    deck.push(build_card(suit.to_string(), special.to_string()));
                 }
             }
 
         }
     }
-    return deck;
-}
+return deck; // Return the deck for use in the card game.
+
 
--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,40 @@ fn main() {
     println!("{}", "Blackjack".bold().cyan());
     gameLoop();
 }
+/*
+ *  This shit should have been commented a long time ago.
+ *  Guess I should be doing it right now before starting any more.
+ *  Also I have like 4 hours until my maths exam so that is cool.
+ *  I'll be quickly revising for it at 6 am I imagine. 
+ *  Not too bothered by it since I should already have 65% on the module.
+ */
 
 fn betting(mut balance: isize) -> (isize, usize) {
     let mut bet = String::new();
-    while String::from(&bet).len() <= 1 {
+    
+    
+    /*
+     *  This section deals with the reading of a bet.
+     *  Currently also handles the deducation of balance but that'll likely
+     *  be shifted to a different function.
+     */
+    while String::from(&bet).len() <= 1 { 
         bet = String::from("");
-        println!("Balance: {}{}", "£".green().bold(), balance.to_string().green().bold());
+        println!("Balance: {}{}", "£".green().bold(),
+            balance.to_string().green().bold()); // Displays the user's current balance.
         println!("Make your bet!:");
+
         std::io::stdin()
             .read_line(&mut bet)
-            .expect("Failed to read line");
+            .expect("Failed to read line"); // Check if the input is even readable.
 
         let bet: usize = match bet 
             .trim()
             .parse::<usize>(){
                 Ok(num) => num,
                 Err(_) => continue,
-            };
+            }; // Removes all whitespace and converts to unsigned integer.
+
            //.expect("{} {}",String::from(bet).red().bold(), "is not a valid number!".red().bold());
     }
     let bet: usize = match bet 
@@ -31,41 +48,61 @@ fn betting(mut balance: isize) -> (isize, usize) {
         .parse::<usize>(){
             Ok(num) => num,
             Err(_) => unreachable!(),
-        }; 
-    balance = balance - (bet as isize);
-    println!("{:?}", bet);
-    let bet: usize = 0;
-    return (balance, bet);
+        }; // This for some reason does what should be happening in the while loop?
+    
+    balance = balance - (bet as isize); // Test deducation, remove this once gameplay is working.          
+    
+    println!("{:?}", bet); // Output the placed bet. Don't think this does anything but debug.
+    let bet: usize = 0; // Resets the bet. Idk why I set this. If the bet is going to be passed.
+    return (balance, bet); 
 }
 
-fn gameLoop(){
-    let mut balance: isize = 100;
-    let cleanDeck = cards::build_deck();
+fn gameLoop(){ // Main gameloop, this is where everything will be called from.
+    let mut balance: isize = 100; // Initial starting balance.                              
+
+    /*
+     *  The building of a clean deck for the start of the game is currently unneeded, whilst the
+     *  resources used to generate the deck are insignificant it could be reduced by first time
+     *  generating a desk and just saving it into a txt file.
+     *  The generation function was more so made so that I didn't have to make a deck myself, and
+     *  so that it could be further expanded to allow for things such as multiple decks to be used
+     *  in a game.
+     */
+    let cleanDeck = cards::build_deck(); // Builds a clean, unshuffled deck. 
+    let shuffledDeck = shuffleDeck(cleanDeck);
     'Gameloop: loop{
-        let bettingResults = betting(balance);
+        let bettingResults = betting(balance); // Retrieves the results from betting.
         balance = bettingResults.0;
         let mut bet: usize = bettingResults.1;
-        println!("{:#?}", shuffleDeck(cleanDeck.clone()));
-        println!("{}", cleanDeck.len());
+        
     }
 }
 
-fn shuffleDeck(reference: Vec<cards::Card>) -> Vec<cards::Card>{
+fn shuffleDeck(reference: Vec<cards::Card>) -> Vec<cards::Card>{ // Self-explanatory
+   /*
+    *  For ease of use down the line the generated deck is not directly modified, instead the deck
+    *  is cloned (in this case it is cloned into 'unshuffled') and the clone is manipulated
+    *  instead. This was done just to prevent having to run the card builder multiple times per
+    *  game. 
+    *  Insignificant in something like this, but I feel like it's a good habit to learn to lower
+    *  processing time on shit hardware, even if it increases the RAM usage.
+    */
     let mut shuffled = Vec::new();
     let mut unshuffled = reference.clone();
     loop {
-        let mut unshuffledLength = unshuffled.len();
+        let mut unshuffledLength = unshuffled.len(); // Use this to make sure the deck contains
+                                                     // cards.
         let pos: usize;
-        if unshuffledLength != 1 {
+        if unshuffledLength != 1 { // If the deck contains more than one card then randomly select
+                                   // a vector position within the deck.
             pos = rand::thread_rng().gen_range(0..unshuffledLength);
         } else {
-            pos = 0;
+            pos = 0;               // Else select the only card.
         }
-        shuffled.push(unshuffled[pos].clone());
-        //println!("{:#?}", unshuffled[pos]);
-        unshuffled.remove(pos);
-        println!("{:#?}, {:#?}", unshuffled.len(),unshuffled);
-        if (unshuffled.len() <= 0) {
+        shuffled.push(unshuffled.remove(pos)); // This should remove the card from unshuffled and
+                                               // move it into the shuffled vector.
+        println!("{:#?}, {:#?}", shuffled.len(),shuffled); // Debug.
+        if (unshuffled.len() <= 0) { // Stop the loop once the deck has been shuffled.
             break
         }
     }
